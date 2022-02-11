93d1e8e776acb3cb5cc92294fa93d64baa1ad1f3
Tuti <josehuerta115@gmail.com>
2022-02-11 14:12:31 -0800

    added grid layout functionality and starting to work on adding book action

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 const bookLibrary = [];
+const books = document.querySelector('#books');
+
+let ex_book = new Book('The Hunger Games', 'Suzanne Collins', '300', true)
+let addBook = document.getElementById('addBook');
+addBook.addEventListener('click', event => {
+    addBookToLibrary(ex_book);
+})
+
 
 function Book(title, author, pageCount, haveRead) {
     this.title = title;
@@ -10,5 +18,15 @@ function Book(title, author, pageCount, haveRead) {
 
 function addBookToLibrary(book) {
     bookLibrary.push(book);
-}
+    console.log(bookLibrary);
+    let entry = document.createElement('div');
+    //let text = document.createTextNode('this is a test this is a test');
+    let title_bold = document.createElement('p');
+    title_bold.style.cssText = 'text-align: center; font-weight: bold;'
+    title_bold.append('Title:')
 
+    entry.style.cssText = 'display: flex; background-color: grey; text-align: center; width: 100%; height: 100%; aspect-ratio: 1;'
+    //book.appendChild(text);
+    entry.appendChild(title_bold);
+    books.appendChild(entry);
+}
